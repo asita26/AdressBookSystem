b8891d873579d5bad129cc16845c7357722fef41
asita26 <asita98890@gmail.com>
2023-11-25 15:25:31 +0530

    Adding code for creating contacts in address book list

---
+++ src/Contact.java
@@ -0,0 +1,14 @@
+public class Contact  {
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String zip;
+        private String phoneNumber;
+        private String email;
+
+
+    }
+
+
