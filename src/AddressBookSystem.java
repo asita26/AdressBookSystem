666d671257fc4c8e1be308e5fd88553073d3406a
asita26 <asita98890@gmail.com>
2023-11-25 15:39:45 +0530

    Refactoring code to take input from user

--- src/AddressBookSystem.java
+++ src/AddressBookSystem.java
@@ -1,39 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-import java.util.ArrayList;
-import java.util.List;
-
 public class AddressBookSystem {
-    private List<Contact> contacts;
-
-    public AddressBookSystem() {
-        this.contacts = new ArrayList<>();
-    }
-
-    public void addContact(Contact contact) {
-
-    }
-
-
-
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
 
         // Create an instance of AddressBookSystem and perform operations
-        AddressBookSystem addressBook = new AddressBookSystem();
+        AddressBook addressBook = new AddressBook();
 
-        // Example: Creating a new contact
+        // Example: Creating a new contact using console input
         Contact newContact = new Contact();
-        newContact.setFirstName("Asita");
-        newContact.setLastName("M");
+        Scanner scanner = new Scanner(System.in);
 
-        // Setting other contact details
+        System.out.print("Enter first name: ");
+        newContact.setFirstName(scanner.nextLine());
+
+        System.out.print("Enter last name: ");
+        newContact.setLastName(scanner.nextLine());
+
+
+
         addressBook.addContact(newContact);
-
-
-        for (Contact contact : addressBook.contacts) {
-            System.out.println(contact.getFirstName() + " " + contact.getLastName());
-            // Display other contact details
-        }
+        addressBook.displayContacts();
     }
 }
