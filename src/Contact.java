5cb831be1d762f861a2a4f9c41de05529b925b25
asita26 <asita98890@gmail.com>
2023-11-26 22:10:07 +0530

    Adding and refactoring code to define getter and setter method for all the contact details

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,4 @@
-public class Contact  {
+public class Contact {
         private String firstName;
         private String lastName;
         private String address;
@@ -8,7 +8,80 @@ public class Contact  {
         private String phoneNumber;
         private String email;
 
+        // Constructors, if needed
 
-    }
+        // Getter and setter methods for firstName
+        public String getFirstName() {
+                return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+                this.firstName = firstName;
+        }
+
+        // Getter and setter methods for lastName
+        public String getLastName() {
+                return lastName;
+        }
+
+        public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
+
+        // Getter and setter methods for address
+        public String getAddress() {
+                return address;
+        }
+
+        public void setAddress(String address) {
+                this.address = address;
+        }
+
+        // Getter and setter methods for city
+        public String getCity() {
+                return city;
+        }
+
+        public void setCity(String city) {
+                this.city = city;
+        }
+
+        // Getter and setter methods for state
+        public String getState() {
+                return state;
+        }
+
+        public void setState(String state) {
+                this.state = state;
+        }
+
+        // Getter and setter methods for zip
+        public String getZip() {
+                return zip;
+        }
+
+        public void setZip(String zip) {
+                this.zip = zip;
+        }
+
+        // Getter and setter methods for phoneNumber
+        public String getPhoneNumber() {
+                return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+                this.phoneNumber = phoneNumber;
+        }
+
+        // Getter and setter methods for email
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+}
+
 
 
